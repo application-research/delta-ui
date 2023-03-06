2b6410eaa4f8ea20cb2ec61bbcc242553f13ffd0
Elijah Seed-Arita <elijaharita@gmail.com>
2023-03-06 10:04:31 -1000

    use more standard indentation pattern

--- components/SceneDatasets.tsx
+++ components/SceneDatasets.tsx
@@ -9,51 +9,44 @@ import * as Utilities from '@common/utilities';
 import Input from '@components/Input';
 
 export default function SceneDatasets(props) {
-  return (
-    <div className={styles.body}>
-      <Input label={props.searchLabel} id="scene-datasets-search" placeholder={props.placeholder} value={props.searchValue} onChange={props.onSearchChange} />
-      {
-        props.state.datasets && 
-        <div className={tableStyles.body}>
-          <div className={tableStyles.header}>
-            <span className={tableStyles.column}>ID</span>
-            <span className={tableStyles.column}>Name</span>
-            <span className={tableStyles.column}>Size</span>
-            <span className={tableStyles.column}>Replication Quota</span>
-            <span className={tableStyles.column}>Start Epoch Delay</span>
-            <span className={tableStyles.column}>Duration</span>
-            <span className={tableStyles.column}>Unsealed</span>
-            <span className={tableStyles.column}>Indexed</span>
-            <span className={tableStyles.fluidColumn}>Wallet</span>
-          </div>
-          {
-            props.state.datasets.map((dataset, i) => {
-              let progress = dataset.bytes_replicated[0] / dataset.bytes_total[0] / dataset.replication_quota;
-
-              return (
-                <div>
-                  <div key={i} className={tableStyles.row}>
-                    <span className={tableStyles.column}>{dataset.ID}</span>
-                    <span className={tableStyles.column}>{dataset.name}</span>
-                    <span className={tableStyles.column}>{Utilities.bytesToSize(dataset.bytes_total[0])}</span>
-                    <span className={tableStyles.column}>{dataset.replication_quota}</span>
-                    <span className={tableStyles.column}>{dataset.delay_start_epoch}</span>
-                    <span className={tableStyles.column}>{dataset.deal_duration} days</span>
-                    <span className={tableStyles.column}>{dataset.unsealed ? "true" : "false"}</span>
-                    <span className={tableStyles.column}>{dataset.indexed ? "true" : "false"}</span>
-                    <span className={tableStyles.fluidColumn}>{dataset.wallet.address}</span>
-                  </div>
-                  <div className={tableStyles.progress}>
-                    <div className={tableStyles.progressBar} style={{ width: `${progress * 100}%` }} />
-                  </div>
-                  <div className={tableStyles.rowButton}>➟ Make storage deals for this dataset</div>
-                  <div className={tableStyles.rowButton}>➟ Attach content</div>
-                </div>
-              )
-            })
-          }
+  return (<div className={styles.body}>
+    <Input label={props.searchLabel} id="scene-datasets-search" placeholder={props.placeholder} value={props.searchValue} onChange={props.onSearchChange} />
+    {props.state.datasets &&
+      <div className={tableStyles.body}>
+        <div className={tableStyles.header}>
+          <span className={tableStyles.column}>ID</span>
+          <span className={tableStyles.column}>Name</span>
+          <span className={tableStyles.column}>Size</span>
+          <span className={tableStyles.column}>Replication Quota</span>
+          <span className={tableStyles.column}>Start Epoch Delay</span>
+          <span className={tableStyles.column}>Duration</span>
+          <span className={tableStyles.column}>Unsealed</span>
+          <span className={tableStyles.column}>Indexed</span>
+          <span className={tableStyles.fluidColumn}>Wallet</span>
         </div>
-      }
-    </div>
-  );
+        {props.state.datasets.map((dataset, i) => {
+          let progress = dataset.bytes_replicated[0] / dataset.bytes_total[0] / dataset.replication_quota;
+
+          return (<div>
+            <div key={i} className={tableStyles.row}>
+              <span className={tableStyles.column}>{dataset.ID}</span>
+              <span className={tableStyles.column}>{dataset.name}</span>
+              <span className={tableStyles.column}>{Utilities.bytesToSize(dataset.bytes_total[0])}</span>
+              <span className={tableStyles.column}>{dataset.replication_quota}</span>
+              <span className={tableStyles.column}>{dataset.delay_start_epoch}</span>
+              <span className={tableStyles.column}>{dataset.deal_duration} days</span>
+              <span className={tableStyles.column}>{dataset.unsealed ? "true" : "false"}</span>
+              <span className={tableStyles.column}>{dataset.indexed ? "true" : "false"}</span>
+              <span className={tableStyles.fluidColumn}>{dataset.wallet.address}</span>
+            </div>
+            <div className={tableStyles.progress}>
+              <div className={tableStyles.progressBar} style={{ width: `${progress * 100}%` }} />
+            </div>
+            <div className={tableStyles.rowButton}>➟ Make storage deals for this dataset</div>
+            <div className={tableStyles.rowButton}>➟ Attach content</div>
+          </div>)
+        })}
+      </div>
+    }
+  </div>);
 }
--- components/SceneProviders.tsx
+++ components/SceneProviders.tsx
@@ -9,32 +9,25 @@ import * as Utilities from '@common/utilities';
 import Input from '@components/Input';
 
 export default function SceneProviders(props) {
-  return (
-    <div className={styles.body}>
-      <Input label={props.providerLabel} id="scene-provider-search" placeholder={props.placeholder} value={props.providerValue} onChange={props.onProviderChange} />
-      {
-        props.state.providers &&
-        <div className={tableStyles.body}>
-          <div className={tableStyles.header}>
-            <span className={tableStyles.column}>Actor ID</span>
-            <span className={tableStyles.fluidColumn}>Replicated Deals</span>
-          </div>
-          {
-            props.state.providers.map((provider, i) => {
-              return (
-                <div>
-                  <div key={i} className={tableStyles.row}>
-                    <span className={tableStyles.column}>{provider.actor_id}</span>
-                    <span className={tableStyles.fluidColumn}>{provider.replications?.length() || 0}</span>
-                  </div>
-                  <div className={tableStyles.rowButton}>➟ Make storage deals</div>
-                  <div className={tableStyles.rowButton}>➟ View details</div>
-                </div>
-              )
-            })
-          }
+  return (<div className={styles.body}>
+    <Input label={props.providerLabel} id="scene-provider-search" placeholder={props.placeholder} value={props.providerValue} onChange={props.onProviderChange} />
+    {props.state.providers &&
+      <div className={tableStyles.body}>
+        <div className={tableStyles.header}>
+          <span className={tableStyles.column}>Actor ID</span>
+          <span className={tableStyles.fluidColumn}>Replicated Deals</span>
         </div>
-      }
-    </div>
-  );
+        {props.state.providers.map((provider, i) => {
+          return (<div>
+            <div key={i} className={tableStyles.row}>
+              <span className={tableStyles.column}>{provider.actor_id}</span>
+              <span className={tableStyles.fluidColumn}>{provider.replications?.length() || 0}</span>
+            </div>
+            <div className={tableStyles.rowButton}>➟ Make storage deals</div>
+            <div className={tableStyles.rowButton}>➟ View details</div>
+          </div>)
+        })}
+      </div>
+    }
+  </div>);
 }
