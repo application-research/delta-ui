7d7ab790561f2fdd3c34c140c2598be028ebe78a
Elijah Seed-Arita <elijaharita@gmail.com>
2023-03-16 15:54:31 -1000

    use SI byte sizes (GiB instead of GB etc)

--- common/utilities.ts
+++ common/utilities.ts
@@ -34,9 +34,9 @@ export const elide = (string, length = 140, emptyState = '...') => {
 export function bytesToSize(bytes: number, decimals: number = 2) {
   if (bytes === 0) return '0 Bytes';
 
-  const k = 1000;
+  const k = 1024;
   const dm = decimals < 0 ? 0 : decimals;
-  const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const sizes = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
 
   const i = Math.floor(Math.log(bytes) / Math.log(k));
 
