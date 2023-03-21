538af1fe254b72fe1cd8921197fd8405d1b55abe
Elijah Seed-Arita <elijaharita@gmail.com>
2023-03-21 11:49:04 -1000

    auto normalize dataset name

--- components/forms/FormNewDataset.tsx
+++ components/forms/FormNewDataset.tsx
@@ -7,6 +7,7 @@ import Input from '@components/Input';
 import Button from '@components/Button';
 import React from 'react';
 import { addDataset } from '@root/data/api';
+import { createSlug } from '@root/common/utilities';
 
 export default function FormNewDataset(props) {
   let [name, setName] = React.useState('');
@@ -39,11 +40,27 @@ export default function FormNewDataset(props) {
     return name.length > 0 && replications > 0 && duration > 0;
   }
 
+  function normalizeName(text: any) {
+    const a = 'æøåàáäâèéëêìíïîòóöôùúüûñçßÿœæŕśńṕẃǵǹḿǘẍźḧ·/_,:;';
+    const b = 'aoaaaaaeeeeiiiioooouuuuncsyoarsnpwgnmuxzh------';
+    const p = new RegExp(a.split('').join('|'), 'g');
+  
+    return text
+      .toString()
+      .toLowerCase()
+      .replace(/\s+/g, '-') // Replace spaces with -
+      .replace(p, (c) => b.charAt(a.indexOf(c))) // Replace special chars
+      .replace(/&/g, '-and-') // Replace & with 'and'
+      .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+      .replace(/\-\-+/g, '-') // Replace multiple - with single -
+      .replace(/^-+/, ''); // Trim - from start of text
+  }
+
   return (
     <Dismissible className={styles.body} onOutsideClick={props.onOutsideClick}>
       <form onSubmit={onSubmit}>
         <h2 className={styles.heading}>New dataset</h2>
-        <Input label="Dataset Name" id="dataset-name" value={name} placeholder="a friendly name" required autoFocus onChange={e => setName(e.target.value)} />
+        <Input label="Dataset Name" id="dataset-name" value={name} placeholder="a friendly name" required autoFocus onChange={e => setName(normalizeName(e.target.value))} />
         <Input type="number" label="Replication Count" value={replications} id="dataset-replications" required onChange={e => setReplications(e.target.value)} />
         <Input type="number" label="Deal Duration (Days)" value={duration} onChange={e => setDuration(e.target.value)} />
         <Input type="checkbox" label="Publish to indexer?" checked={indexed} id="dataset-indexed" onChange={e => setIndexed(e.target.checked)} />
