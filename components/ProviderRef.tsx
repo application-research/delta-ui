233e8037982a07e08848b363d5e3637f94041331
Elijah Seed-Arita <elijaharita@gmail.com>
2023-03-22 09:09:29 -1000

    fix ProviderRef displaying wrong text

--- components/ProviderRef.tsx
+++ components/ProviderRef.tsx
@@ -1,7 +1,7 @@
 export default function ProviderRef(props) {
   return (
     <span>
-      props.providerID <a href={`https://filfox.info/en/address/${props.providerID}`} target='_blank'>filfox</a>
+      {props.providerID} <a href={`https://filfox.info/en/address/${props.providerID}`} target='_blank'>filfox</a>
     </span>
   )
 }
