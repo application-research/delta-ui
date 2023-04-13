06cf43dad403af81748ee7200671aac5df22d218
Elijah Seed-Arita <elijaharita@gmail.com>
2023-04-13 13:20:27 -1000

    clean up layout and convert all non-root pixel font sizes to rem

--- components/scenes/SceneReplications.tsx
+++ components/scenes/SceneReplications.tsx
@@ -19,8 +19,8 @@ export default function SceneReplications(props) {
           <div className={tableStyles.header}>
             <div className={tableStyles.column}>Dataset</div>
             <div className={tableStyles.column}>Status</div>
-            <div className={tableStyles.column}>Deal Time</div>
             <div className={tableStyles.column}>Provider ID</div>
+            <div className={tableStyles.fluidColumn}>Deal Time</div>
             <div className={tableStyles.fluidColumn}>Proposal CID</div>
             <div className={tableStyles.fluidColumn}>Piece CID (CommP)</div>
             <div className={tableStyles.fluidColumn}>Message</div>
@@ -41,8 +41,8 @@ export default function SceneReplications(props) {
                   <div className={tableStyles.row}>
                     <div className={tableStyles.column}>{replication.content.dataset_name}</div>
                     <div className={tableStyles.column}>{replication.status}</div>
-                    <div className={tableStyles.column}>{replication.deal_time}</div>
                     <div className={tableStyles.column}><ProviderRef providerID={replication.provider_actor_id} /></div>
+                    <div className={tableStyles.fluidColumn}>{new Date(Date.parse(replication.deal_time)).toUTCString()}</div>
                     <div className={tableStyles.fluidColumn}>{replication.proposal_cid}</div>
                     <div className={tableStyles.fluidColumn}>{replication.content_commp}</div>
                     <div className={tableStyles.fluidColumn}>{replication.delta_message}</div>
