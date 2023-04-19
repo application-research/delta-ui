21824b4ec191e2ba99e2e8c5d42c7d22d39fc901
Elijah Seed-Arita <elijaharita@gmail.com>
2023-04-19 17:32:15 +0000

    forgot a file

---
+++ components/Feedback.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+
+import styles from './Feedback.module.scss';
+
+export default function Feedback(props: {
+  children?: string,
+  type?: 'success' | 'error'
+}) {
+  if (!props.type) return null;
+  
+  const options = {
+    'success': {
+      icon: '✓',
+      style: styles.success
+    },
+    'error': {
+      icon: '⚠',
+      style: styles.error
+    }
+  };
+
+  let option = options[props.type];
+  
+  return (
+    <div className={option.style}>
+      <div className={styles.icon}>{option.icon}</div>
+      {props.children && <div className={styles.message}>{props.children}</div>}
+    </div>
+  )
+}
+
